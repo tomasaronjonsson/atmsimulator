6a1ab1139762307d06f33a8532f3b12c471d8676
tomasaronjonsson <tomasaronjonsson@gmail.com>
2014-09-10 10:44:10 +0200

    sp1 - adding files to model

---
+++ ATM Simulator/ATMS Model/Scenario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMS_Model
+{
+    class Scenario
+    {
+        public List<Track> tracks;
+    }
+}
---
+++ ATM Simulator/ATMS Model/Track.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMS_Model
+{
+    class Track
+    {
+        public int trackID;
+        public List<Plot> plots;
+
+
+
+
+        //skeleton
+        Plot getNow()
+        {
+            return new Plot();
+        }
+    }
+}
