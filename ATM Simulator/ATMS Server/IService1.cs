a0d98a2a63db721d32c5b55811d41baa984535a0
Alexandru Cioanca Bachelor <alexandru.cioanca@gmail.com>
2014-09-12 09:59:52 +0200

    server renamed

--- ATM Simulator/ATMS Server/IService1.cs
+++ ATM Simulator/ATMS Server/IService1.cs
@@ -9,7 +9,7 @@ namespace ATMS_Server
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IServerInterface
     {
         [OperationContract]
         string GetData(int value);
--- ATM Simulator/ATMS Server/Service1.cs
+++ ATM Simulator/ATMS Server/Service1.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace ATMS_Server
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class MainSimulation : IServerInterface
     {
         public string GetData(int value)
         {
