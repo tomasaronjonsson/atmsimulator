df55ebe47c910db2709cee128fd73e7a6d111d19
Alexandru Cioanca Bachelor <alexandru.cioanca@gmail.com>
2014-09-12 10:17:19 +0200

    removing the model

---
+++ ATM Simulator/ATMS Server/ScenarioLayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMS_Model
+{
+    class ScenarioLayer
+    {
+        public int layerLevel;
+        public Scenario scenario;
+        public int timeOffSetSeconds;
+
+    }
+}
