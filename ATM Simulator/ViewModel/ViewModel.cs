318284ae45f4dd996eedcbf416f5c0aa759e2386
Alexandru Cioanca Bachelor <alexandru.cioanca@gmail.com>
2014-10-06 09:41:02 +0200

    foldering

---
+++ ATM Simulator/ViewModel/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ViewModel
+{
+    public class ViewModel : ViewModelBase
+    {
+        Scenario scenario;
+        Track track;
+        Plot plot;
+
+
+    }
+}
