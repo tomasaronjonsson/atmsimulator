adfb4103064fe5630376ef240098429159ecd632
Alexandru Cioanca Bachelor <alexandru.cioanca@gmail.com>
2014-11-13 20:04:00 +0100

    s6. SERVER cleanup

--- ATM Simulator/ATMS Server/MainSimulation.cs
+++ ATM Simulator/ATMS Server/MainSimulation.cs
@@ -127,12 +127,12 @@ namespace ATMS_Server
             {
                 //Check the client registration
                 checkIfRegistered();
-                
+
                 //Create the new scenario
                 mainScenario = new Scenario();
 
                 //Populate the scenario with the Test data
-                if(mainScenario != null)
+                if (mainScenario != null)
                     mainScenario = Test.populateInitialScenario();
 
                 //Update the available ID
@@ -206,11 +206,11 @@ namespace ATMS_Server
                 //Add the track to the main scenario
                 mainScenario.tracks.Add(t);
 
-                //notify the clients of the newly added track
+                //Call back towards the client with the reply
                 clients.ForEach(delegate(IClientCallbackInterface callback)
-                {
-                    callback.notifyNewTrack(t);
-                }
+                    {
+                        callback.notifyNewTrack(t);
+                    }
                 );
             }
             catch (Exception e)
@@ -218,7 +218,7 @@ namespace ATMS_Server
                 //Catch and report the exception
                 debugMessage("Failed to create a new track", e);
                 throw new Exception("ATMS-MainSimulation-0004: Failed to create a new track");
-            }            
+            }
         }
 
         /**
@@ -226,164 +226,184 @@ namespace ATMS_Server
          * */
         public void removeTrack(Track t)
         {
-            //Check the client registration
-            checkIfRegistered();
-
-            //Validate the input
-            if (t != null)
+            try
             {
-                //Remove the track 
-                mainScenario.tracks.Remove(t);
+                //Check the client registration
+                checkIfRegistered();
 
-                //notify all the clients 
-                clients.ForEach(
-                 delegate(IClientCallbackInterface callback)
-                 {
-                     callback.notifyRemoveTrack(t);
+                //Validate the input
+                if (t != null)
+                {
+                    //Remove the track 
+                    mainScenario.tracks.Remove(t);
 
-                 });
+                    //Call back towards the client with the reply
+                    clients.ForEach(delegate(IClientCallbackInterface callback)
+                        {
+                            callback.notifyRemoveTrack(t);
+                        }
+                    );
+                }
+            }
+            catch (Exception e)
+            {
+                //Catch and report the exception
+                debugMessage("Failed to remove the track", e);
+                throw new Exception("ATMS-MainSimulation-0005: Failed to remove the track");
             }
         }
 
         /**
-         * sprint 6
+         * Edit a track
          * */
         public void editTrack(Track t)
         {
-
-            //lets check if the client is registered
-            checkIfRegistered();
-            //check if null else we skip
-            if (t != null)
+            try
             {
-                //finding the track to be changed
-                Track trackToBeChanged = mainScenario.tracks.First(x => x.Equals(t));
+                //Check the client registration
+                checkIfRegistered();
 
-                //check if we found something
-                if (trackToBeChanged != null)
+                //Validate the input
+                if (t != null)
                 {
-                    //edit what we found
-                    trackToBeChanged.edit(t);
+                    //Reference the track that will be changed
+                    Track trackToBeChanged = mainScenario.tracks.First(x => x.Equals(t));
 
-
-                    //notify the other clients
-                    clients.ForEach(
-                 delegate(IClientCallbackInterface callback)
-                 {
-                     callback.notifyEditedTrack(t);
-
-                 });
-                }
-            }
-        }
-
-        public void createNewPlot(Plot p)
-        {
-            //lets check if the client is registered
-            checkIfRegistered();
-            //check if the value incoming is Null
-            if (p != null)
-            {
-                //make a reference to the track that we need to add to (the selectedTrack)
-                Track trackToBeAddedTo = mainScenario.tracks.First(x => x.trackID == p.trackID);
-                //Add the plot
-                trackToBeAddedTo.plots.Add(p);
-
-                //notify the clients of the newly added track
-                clients.ForEach(
-                    delegate(IClientCallbackInterface callback)
+                    //Validate the track
+                    if (trackToBeChanged != null)
                     {
-                        callback.notifyNewPlot(p);
-                    });
-            }
-        }
+                        //Edit the track
+                        trackToBeChanged.edit(t);
 
-        public void removePlot(Plot p)
-        {
-            checkIfRegistered();
-            if (p != null)
-            {
-                Track trackToBeRemovedFrom = mainScenario.tracks.First(x => x.trackID == p.trackID);
-                //remove the plot
-                trackToBeRemovedFrom.plots.Remove(p);
-
-                clients.ForEach(
-                 delegate(IClientCallbackInterface callback)
-                 {
-                     callback.notifyRemovePlot(p);
-                 });
-            }
-        }
-
-        public void editPlot(Plot p)
-        {
-            checkIfRegistered();
-            if (p != null)
-            {
-                //finding the track to be changed
-                Track trackToLookInto = mainScenario.tracks.First(x => x.trackID == p.trackID);
-
-                //check if we found something
-                if (trackToLookInto != null)
-                {
-                    //edit what we found
-                    Plot plotToBeChanged = trackToLookInto.plots.First(x => x.trackID == p.trackID);
-
-                    //check if we found something and then edit it
-                    if (plotToBeChanged != null)
-                        plotToBeChanged.edit(p);
-
-
-                    //notify the other clients
-                    clients.ForEach(
-                 delegate(IClientCallbackInterface callback)
-                 {
-                     callback.notifyEditedPlot(p);
-                 });
+                        //Call back towards the client with the reply
+                        clients.ForEach(delegate(IClientCallbackInterface callback)
+                            {
+                                callback.notifyEditedTrack(t);
+                            }
+                        );
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                //Catch and report the exception
+                debugMessage("Failed to edit the track", e);
+                throw new Exception("ATMS-MainSimulation-0006: Failed to edit the track");
+            }
         }
-
-
-
-
-
-
-
-        #endregion
 
         /*
-         * The implementation of the callbacks to the callbackinteerface in the duplex contract
+         * Create a new plot
          * */
-        #region Communication with client
-
-
-
-        public void sendNewScenario()
+        public void createNewPlot(Plot p)
         {
-            foreach (IClientCallbackInterface entry in clients)
+            try
             {
-                try
-                {
-                    //Handle the client callbacks, 1st argument is the function, 2nd is the client and 3rd is the client list
-                    clients.ForEach(
-              delegate(IClientCallbackInterface callback)
-              {
-                  callback.notifyNewScenario(mainScenario);
+                //Check the client registration
+                checkIfRegistered();
 
-              });
-                }
-                catch (Exception e)
+                //Validate the input
+                if (p != null)
                 {
-                    //handle that the scenario is to big and can't be sent like this 
-                    debugMessage("Failed to send new scenario", e);
+                    //Reference the track that will hold the new plot
+                    Track trackToBeAddedTo = mainScenario.tracks.First(x => x.trackID == p.trackID);
 
+                    //Add the new plot
+                    trackToBeAddedTo.plots.Add(p);
+
+                    //Call back towards the client with the reply
+                    clients.ForEach(delegate(IClientCallbackInterface callback)
+                        {
+                            callback.notifyNewPlot(p);
+                        }
+                    );
                 }
+            }
+            catch (Exception e)
+            {
+                //Catch and report the exception
+                debugMessage("Failed to create the plot", e);
+                throw new Exception("ATMS-MainSimulation-0007: Failed to create the plot");
             }
         }
 
+        /*
+         * Remove a plot
+         * */
+        public void removePlot(Plot p)
+        {
+            try
+            {
+                //Check the client registration
+                checkIfRegistered();
 
+                //Validate the input
+                if (p != null)
+                {
+                    //Reference the track where the plot will be removed from
+                    Track trackToBeRemovedFrom = mainScenario.tracks.First(x => x.trackID == p.trackID);
 
+                    //Remove the plot
+                    trackToBeRemovedFrom.plots.Remove(p);
+
+                    //Call back towards the client with the reply
+                    clients.ForEach(delegate(IClientCallbackInterface callback)
+                        {
+                            callback.notifyRemovePlot(p);
+                        }
+                    );
+                }
+            }
+            catch (Exception e)
+            {
+                //Catch and report the exception
+                debugMessage("Failed to remove the plot", e);
+                throw new Exception("ATMS-MainSimulation-0008: Failed to remove the plot");
+            }
+        }
+
+        /*
+         * Edit a plot
+         * */
+        public void editPlot(Plot p)
+        {
+            try
+            {
+                //Check the client registration
+                checkIfRegistered();
+
+                //Validate the input
+                if (p != null)
+                {
+                    //Reference the track where the plot will be removed from
+                    Track trackToLookInto = mainScenario.tracks.First(x => x.trackID == p.trackID);
+
+                    //Validate the track
+                    if (trackToLookInto != null)
+                    {
+                        //Reference the plot that will be edited
+                        Plot plotToBeChanged = trackToLookInto.plots.First(x => x.trackID == p.trackID);
+
+                        //Validate the plot
+                        if (plotToBeChanged != null)
+                            plotToBeChanged.edit(p);
+
+                        //Call back towards the client with the reply
+                        clients.ForEach(delegate(IClientCallbackInterface callback)
+                            {
+                                callback.notifyEditedPlot(p);
+                            }
+                        );
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //Catch and report the exception
+                debugMessage("Failed to edit the plot", e);
+                throw new Exception("ATMS-MainSimulation-0009: Failed to edit the plot");
+            }
+        }
 
         #endregion
     }
