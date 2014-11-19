477c723340c892aada9530b82db4e7cf78227f62
Alexandru Cioanca Bachelor <alexandru.cioanca@gmail.com>
2014-11-19 21:32:41 +0100

    s7. small fix

--- ATM Simulator/View/MainWindow.xaml.cs
+++ ATM Simulator/View/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using DevExpress.Xpf.Map;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using ViewModel;
 
 namespace View
 {
@@ -18,7 +19,25 @@ namespace View
 
         private void VectorLayer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            //GeoPoint geoPt = this.theMap.Layers[0].ScreenToGeoPoint(e.GetPosition(this.theMap));
+            GeoPoint geoPt = this.theMap.Layers[0].ScreenToGeoPoint(e.GetPosition(this.theMap));
+
+            ContextMenu contextmenu = new ContextMenu();
+            theMap.ContextMenu = contextmenu;
+
+            MenuItem mi = new MenuItem();
+            mi.Header = "Create Plot";
+            mi.MouseLeftButtonDown += new MouseButtonEventHandler(createPlot);
+            contextmenu.Items.Add(mi);
+        }
+
+        private void createPlot(object sender, MouseButtonEventArgs e)
+        {
+            /*var vm = (SimulationViewModel)DataContext;
+
+            if (vm.CreateNewPlot.CanExecute(vm.tracks.Count > 0))
+            {
+                vm.CreateNewPlot.Execute(null);
+            }*/
         }
     }
 }
